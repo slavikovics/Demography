656073da7c3b2bcebb4b3c58fac06f454c8eddc7
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-09-20 19:21:45 +0300

    Building years

--- belstat_parser/population.py
+++ belstat_parser/population.py
@@ -2,16 +2,14 @@ import requests
 import json
 
 
+def build_years(start_year :int, end_year :int):
+    return [year for year in range(start_year, end_year + 1)]
+
+
 def get_population():
     url = 'https://dataportal.belstat.gov.by/osids-public-api/sdmx-api/indicator/values/SDMX-JSON/10101100004'
     body_scheme = {
-        "years": [
-            2024,
-            2023,
-            2022,
-            2021,
-            2020
-        ],
+        "years": build_years(2020, 2025),
         "periodicities": [],
         "units": [
             "210"
@@ -45,7 +43,7 @@ def get_population():
     resp = requests.post(url=url, json=body_scheme)
 
     if resp.status_code == 200:
-        with open('population.json', 'x', encoding='utf-8') as f:
+        with open('population.json', 'w', encoding='utf-8') as f:
             f.write(resp.text)
 
     else:
