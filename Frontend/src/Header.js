18d1c86d8468b14f7da5fde769c5969d66aad36a
slavikovics <slavikovics@outlook.com>
2025-10-28 05:16:21 +0300

    Updating header

--- Frontend/src/Header.js
+++ Frontend/src/Header.js
@@ -1,9 +1,9 @@
 function Header() {
   return (
     <header className="App-header">
-      <h2 className="HeaderTitle">Демография Республики Беларусь</h2>
+      <h2 className="HeaderTitle">Belarusian Demography</h2>
       <a className="Header-item" href="https://github.com/slavikovics/Demography">Github</a>
-      <a className="Header-item" href="https://dataportal.belstat.gov.by/osids/home-page">Данные</a>
+      <a className="Header-item" href="https://dataportal.belstat.gov.by/osids/home-page">Belstat Dataportal</a>
     </header>
   );
 }
